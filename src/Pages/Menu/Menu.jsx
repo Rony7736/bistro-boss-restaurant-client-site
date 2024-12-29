24fc6c1c8fd984d03daed6ae4833d76dd11e8173
Rony7736 <ronychakrabarty99@gmail.com>
2024-12-30 02:01:13 +0600

    added parallax in menu page and create a components

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,5 +1,9 @@
 
 import { Helmet } from 'react-helmet-async';
+import Cover from '../Shared/Cover/Cover';
+
+import menuImg from "../../assets/menu/menu-bg.jpg"
+import PopularMenu from '../Home/PopolarMenu/PopularMenu';
 
 const Menu = () => {
     return (
@@ -9,7 +13,14 @@ const Menu = () => {
                 <title>Bistro Boss | Menu</title>
             </Helmet>
 
-            menu
+            <Cover img={menuImg} title="our menu"></Cover>
+            <PopularMenu></PopularMenu>
+
+            <Cover img={menuImg} title="our menu"></Cover>
+            <PopularMenu></PopularMenu>
+            
+            <Cover img={menuImg} title="our menu"></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
